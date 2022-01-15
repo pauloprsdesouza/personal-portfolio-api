6233cdb824f695ddd93eb2844e2db8b4899f6822
pauloprsdesouza <paulo.prsdesouza@gmail.com>
2022-01-14 21:30:17 -0300

    Add partial tests on ListPostTest

--- src/Portfolio.Tests/Functional/Posts/ListPostsTest.cs
+++ src/Portfolio.Tests/Functional/Posts/ListPostsTest.cs
@@ -4,6 +4,8 @@ using Xunit;
 using Portfolio.Api.Models.Posts;
 using System.Text.Json;
 using Portfolio.Api.Configuration;
+using System.Net;
+using System.Text;
 
 namespace Portfolio.Tests.Functional.Posts
 {
@@ -22,14 +24,13 @@ namespace Portfolio.Tests.Functional.Posts
         public async Task ShouldCreatePost()
         {
             PostRequest post = new PostRequest();
-            post.Title = "Teste";
-                post.Title = "Title";
-                post.Subtitle = "Subtitle";
-                post.CategoryId = "01FPSZ1HQYS0G5N44GA43KZSKZ";
-                post.Status = "P";
-                post.FrontImageUrl = "https://";
-                post.ReadingTime = "12";
-                post.Content = "Content";
+            post.Title = "Title";
+            post.Subtitle = "Subtitle";
+            post.CategoryId = "01FPSZ1HQYS0G5N44GA43KZSKZ";
+            post.Status = "P";
+            post.FrontImageUrl = "https://";
+            post.ReadingTime = "12";
+            post.Content = "Content";
 
             var response = await _client.PostJsonAsync("/posts", post);
 
@@ -45,6 +46,45 @@ namespace Portfolio.Tests.Functional.Posts
             Assert.Equal(post.FrontImageUrl, postResponse.FrontImageUrl);
             Assert.Equal(post.ReadingTime, postResponse.ReadingTime);
             Assert.Equal(post.Content, postResponse.Content);
+        }
+
+        [Fact]
+        public async Task ShouldNotCreatePostBadRequest()
+        {
+            PostRequest post = new PostRequest();
+            post.Title = "Title";
+            post.Subtitle = "Subtitle";
+            post.CategoryId = "01FPSZ1HQYS0G5N44GA43KZSKZ";
+            post.Status = "P";
+            post.FrontImageUrl = "https://";
+            post.ReadingTime = "12";
+
+            var response = await _client.PostJsonAsync("/posts", post);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ShouldNotCreatePostMaxLenghBadRequest()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < 151; i++)
+            {
+                sb.Append("*");
+            }
+
+            PostRequest post = new PostRequest();
+            post.Title = sb.ToString();
+            post.Subtitle = "Subtitle";
+            post.CategoryId = "01FPSZ1HQYS0G5N44GA43KZSKZ";
+            post.Status = "P";
+            post.FrontImageUrl = "https://";
+            post.ReadingTime = "12";
+            post.Content = "Content";
+
+            var response = await _client.PostJsonAsync("/posts", post);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
     }
 }
