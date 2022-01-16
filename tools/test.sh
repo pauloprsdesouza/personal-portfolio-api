e213378fbe515887676dedb945762c1ea9c84dce
pauloprsdesouza <paulo.prsdesouza@gmail.com>
2022-01-16 13:21:13 -0300

    Update test.sh

--- tools/test.sh
+++ tools/test.sh
@@ -10,13 +10,15 @@ test_project_dir="Portfolio.Tests"
 cd $solution_dir/$test_project_dir
 
 dotnet test \
-  --collect:"XPlat Code Coverage"  \
-  --results-directory:"coverage" \
-
-dotnet new tool-manifest
-dotnet tool install --local dotnet-reportgenerator-globaltool
+  /p:AltCover="true" \
+  /p:AltCoverForce="true" \
+  /p:AltCoverOpenCover="true" \
+  /p:AltCoverXmlReport="coverage/opencover.xml" \
+  /p:AltCoverInputDirectory="$api_project_dir" \
+  /p:AltCoverAttributeFilter="ExcludeFromCodeCoverage" \
+  /p:AltCoverAssemblyExcludeFilter="System(.*)|xunit|$test_project_dir|$api_project_dir.Views"
 
 dotnet reportgenerator \
-  "-reports:coverage/coverage.opencover.xml" \
-  "-reporttypes:Html" \
-  "-targetdir:coverage-report/"
+  "-reports:coverage/opencover.xml" \
+  "-reporttypes:Html;HtmlSummary" \
+  "-targetdir:coverage/report"
