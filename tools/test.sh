dde0459885d4ade0c0c02836a8d3211595f66d2a
pauloprsdesouza <paulo.prsdesouza@gmail.com>
2022-01-15 22:53:23 -0300

    Update CI Config

---
+++ tools/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+basedir="$(dirname $0)/.."
+solution_dir="$basedir/src"
+api_project_dir="Portfolio.Api"
+test_project_dir="Portfolio.Tests"
+
+cd $solution_dir/$test_project_dir
+
+dotnet test \
+  /p:AltCover="true" \
+  /p:AltCoverForce="true" \
+  /p:AltCoverOpenCover="true" \
+  /p:AltCoverXmlReport="coverage/opencover.xml" \
+  /p:AltCoverInputDirectory="$api_project_dir" \
+  /p:AltCoverAttributeFilter="ExcludeFromCodeCoverage" \
+  /p:AltCoverAssemblyExcludeFilter="System(.*)|xunit|$test_project_dir|$api_project_dir.Views"
+
+dotnet reportgenerator \
+  "-reports:coverage/opencover.xml" \
+  "-reporttypes:Html;HtmlSummary" \
+  "-targetdir:coverage/report"
